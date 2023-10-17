821c6a671e3b71595f52484a335e30e28b611486
rudgus327 <rudgus327@gmail.com>
2023-10-17 23:09:34 +0900

    test 중

--- index.mjs
+++ index.mjs
@@ -6,13 +6,7 @@ export { lChecker };
 const checker = new LinkChecker()
 
 async function lChecker(url){
-// event listeners
-// checker.on('link',(link)=>{
-//     console.log(link);
-// }) 
-// checker.on('pagestart',(url)=>{
-//     console.log(url);
-// })
+
 console.log("checker_test:"+url);
     const LOGGER_MAP ={
         OK:chalk.green('.'),
@@ -22,29 +16,40 @@ console.log("checker_test:"+url);
 
     let brokenlinks = [];
 
+    var cnt = 0;
     checker.on('link',(link)=>{
-        process.stdout.write(LOGGER_MAP[link.state]);
-        if(link.state === "BROKEN") brokenlinks.push(link);
+        cnt+=1;
+        console.log("file+_path("+cnt+"):::"+link.parent);
+        if(link.state === "BROKEN") {
+            console.log("BROKE!!!!");
+            brokenlinks.push(link);
+
+
+        }
     }) 
     let pagesChecked = [];
     checker.on('pagestart',(url)=>{
         pagesChecked.push(url);
+        console.log("pagestart:"+pagesChecked)
     })
 
-    await checker.check({path: url, recurse :true,concurrency :2  });
+    await checker.check({path: url, recurse :true,concurrency :2});
     var result = {};
     var resultMsg = "success";
     var arr = [];
-    
+    console.log("checking start");
     if(brokenlinks.length > 0){
         console.log("");
         console.log(`Found ${brokenlinks.length} broken links:`);
         for(const brokenlink of brokenlinks){
             var linkData = {};
             console.log(brokenlink);
+            var details = brokenlink.failureDetails[0];
+            var msg = details.status == 0 ? details.statusText :details.status+" "+ details.statusText;
+            var status_text = "";
+            
             linkData.url = brokenlink.url;    
-            linkData.status = brokenlink.status;    
-            linkData.state = brokenlink.state;    
+            linkData.status = msg;    
             linkData.parent = brokenlink.parent;    
             linkData.source = new URL(brokenlink.parent).pathname;    
             
